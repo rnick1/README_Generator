7d686e36a55bf3b90d28c545dd0680b5d6099071
rnick1 <rnick0260@gmail.com>
2021-02-19 14:53:44 -0800

    Added notes from the files included in the assignment.

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { }
+
+// TODO: Create a function to initialize app
+function init() { }
+
+// Function call to initialize app
+init();
+
+
+// (Student Note): Included in a 'generateMarkdown.js" file:
+
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) { }
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) { }
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) { }
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+    return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
