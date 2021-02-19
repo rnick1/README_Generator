8d8197c72a236df6c62df3ea3e22dbbf24d5fda8
rnick1 <rnick0260@gmail.com>
2021-02-19 15:23:47 -0800

    Added notes for the prompts that I will use.

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
+// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Include packages needed for this application
-
 // TODO: Create an array of questions for user input
-const questions = [];
+// Inputs: (Questions section(User's name, User's GitHub username, Users's email address)), Project title, Project Description, User Story, Challenges, Plans for Future Development,
+// Resources, Contributors, Installation Instructions, Usage Instructions,
+// Confirm: State of deployment (production/deployed), 
+// Table of Contents - generated from the information provided by the user,    
+// Checkbox: License (user selects the relevant license)
+const questions = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input',
+            name: 'location',
+            message: 'Where are you from?',
+        },
+        {
+            type: 'input',
+            name: 'hobby',
+            message: 'What is your favorite hobby?',
+        },
+        {
+            type: 'input',
+            name: 'food',
+            message: 'What is your favorite food?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username',
+        },
+        {
+            type: 'input',
+            name: 'linkedin',
+            message: 'Enter your LinkedIn URL.',
+        },
+    ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
