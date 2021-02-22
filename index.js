2d81e1efeea788ab54efa64b7172a908ef00b5fd
rnick1 <rnick0260@gmail.com>
2021-02-21 19:02:43 -0800

    Fixed problems with the order of questions and spacing issues in the generateMarkdown function.

--- index.js
+++ index.js
@@ -36,12 +36,6 @@ const questions = () =>
         },
         {
             type: 'input',
-            name: 'contributors',
-            message: 'Please enter the GitHub usernames of this project\'s contributors',
-            default: '',
-        },
-        {
-            type: 'input',
             name: 'installation',
             message: 'Please enter the installation instructions here',
             default: '',
@@ -50,6 +44,12 @@ const questions = () =>
             type: 'input',
             name: 'usage',
             message: 'Please enter usage instructions here',
+            default: '',
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'Please enter the GitHub usernames of this project\'s contributors',
             default: '',
         },
         // {
@@ -64,10 +64,10 @@ const generateMarkdown = (answers) =>
     `# ${answers.title}
 ## by: ${answers.username}
 ### Project Description: ${answers.description}
-###Installation: ${answers.installation}
-###Usage: ${answers.usage}
-###Contributors: ${answers.contributors}
-###If you have any questions, please contact ${answers.name} at: ${answers.email}`
+### Installation: ${answers.installation}
+### Usage: ${answers.usage}
+### Contributors: ${answers.contributors}
+### If you have any questions, please contact ${answers.name} at: ${answers.email}`
 
 const init = () => {
     questions().then((answers) => {
@@ -87,39 +87,39 @@ init();
 
 
 
-// From the homework files:
+// // From the homework files:
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFileSync(fileName, data);
-}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
+//     fs.writeFileSync(fileName, data);
+// }
 
-// TODO: Create a function to initialize app
-function init() { }
+// // TODO: Create a function to initialize app
+// function init() { }
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
 
 
-// (Student Note): Included in a 'generateMarkdown.js" file:
+// // (Student Note): Included in a 'generateMarkdown.js" file:
 
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) { }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) { }
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) { }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) { }
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) { }
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-    return `# ${data.title}
+// // TODO: Create a function to generate markdown for README
+// function generateMarkdown(data) {
+//     return `# ${data.title}
 
-`;
-}
+// `;
+// }
 
-module.exports = generateMarkdown;
+// module.exports = generateMarkdown;
